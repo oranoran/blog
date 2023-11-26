ac8bffbee28c3a46d26be172416ce104ba28aeb6
Oran Epelbaum <oran@justwndr.com>
2023-11-26 21:36:37 +0200

    added a robots.txt file so the sitemap can be found

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.epelbaum.me',
-	integrations: [mdx(), sitemap()],
+  site: 'https://www.epelbaum.me',
+  integrations: [mdx(), sitemap(), robotsTxt()]
 });
