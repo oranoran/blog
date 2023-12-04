f58505167d1e73cbc45548ae01a9b75ddf03e6f0
Oran Epelbaum <oran@justwndr.com>
2023-12-04 10:00:07 +0200

    adding google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import robotsTxt from "astro-robots-txt";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.epelbaum.me',
-  integrations: [mdx(), sitemap(), robotsTxt()]
+  integrations: [mdx(), sitemap(), robotsTxt(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })]
 });
