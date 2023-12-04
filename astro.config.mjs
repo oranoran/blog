df0e1f2a74fbb8699f328cd8e6774e0b1d10d337
Oran Epelbaum <oran@justwndr.com>
2023-12-04 12:30:24 +0200

    adding @astro/prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from "astro-robots-txt";
-
 import partytown from "@astrojs/partytown";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.epelbaum.me',
   integrations: [mdx(), sitemap(), robotsTxt(), partytown({
     config: {
-      forward: ["dataLayer.push"],
-    },
-  })]
+      forward: ["dataLayer.push"]
+    }
+  }), prefetch()]
 });
